e63fbe758cf7495bee3a90f93a13c4340d01244a
Axel Díaz <axeldavid_ds@hotmail.com>
2023-03-25 15:10:37 -0600

    Creacion entidad Cliente
    Se creo la entidad cliente

---
+++ src/models/ClienteModel.js
@@ -0,0 +1,33 @@
+import conn from '../db/db.js';
+import { DataTypes } from 'sequelize';
+
+const Cliente = conn.define('Cliente', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: true
+  },
+  nombre: {
+    type: DataTypes.STRING(30),
+    allowNull: true
+  },
+  edad: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  telefono: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  tipo_compra: {
+    type: DataTypes.STRING(30),
+    allowNull: true
+  },
+  fecha_compra: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+});
+
+export default Cliente
