363a8e4ffd12bb886685453843d8ce1bb504c5bc
Axel Díaz <axeldavid_ds@hotmail.com>
2023-03-26 21:56:48 -0600

    CRUD_Cliente
    -Se creo el controlador de la entidad Cliente
    -Se crearon los métodos CRUD de la entidad Cliente
    -Se llamaron los métodos en la ruta de la entidad Cliente
    Es seguro eliminar la branch tras el merge

--- src/routes/cliente.js
+++ src/routes/cliente.js
@@ -1,17 +1,66 @@
 import { Router } from "express";
+import clienteController from '../controllers/clienteController.js'
 
-const cliente = Router()
+const cliente = Router();
+const {
+        getClienteById,
+        createCliente,
+        getClientes,
+        updateCliente,
+        deleteCliente} = clienteController()
 
-cliente.get('/',(req,resp) =>{
-    resp.status(200).json({message:"dentro de cliente"})
+//Get all
+cliente.get('/',async(req,resp) =>{
+    try{
+        const {clis} = await getClientes();
+        resp.status(200).json(clis)
+    } catch(error){
+        resp.json(error)
+    } 
 })
 
-cliente.post('/',(req,resp) => resp.send("Actualizando "))
+//Post
+cliente.post('/',async(req,resp) => {
 
-cliente.get('/:id',(req,resp) => resp.send("Obteniendo "))
+    const data = req.body;
+    const {newCliente} = createCliente(data)
+    try{
+        await newCliente.save()
+        resp.status(201).json(newCliente)
+    }catch(error){
+        resp.json(error)
+    }
+})
 
-cliente.put('/:id',(req,resp) => resp.send("Poniendo "))
+//Get by id
+cliente.get('/:id',async(req,resp) => {
+    try{
+        const{cli} = await getClienteById(req);
+        resp.status(200).json(cli.dataValues)
+    }catch(error){
+        resp.json(error)
+    }
+})
 
-cliente.delete('/:id',(req,resp) => resp.send("Borrando "))
+//Put
+cliente.put('/:id',async(req,resp) => {
+    try{
+        const{cli} = await updateCliente(req);
+        await cli.save();
+        resp.status(200).json(cli.dataValues);
+    } catch(error){
+        resp.json(error)
+    }
+})
+
+//Delete
+cliente.delete('/:id',async(req,resp) => {
+    try{
+        await deleteCliente(req);
+        resp.status(204).json({message:"elemento borrado correctamente"});
+    }catch(error){
+        resp.json(error);
+    }
+})
 
 export default cliente
