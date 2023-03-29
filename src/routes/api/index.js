a1fc1671647ac83db34a14f35c21794b28055ad1
DarlingOlvera <dmop0600@gmail.com>
2023-03-28 22:24:35 -0600

    Prueba de la ruta principal y endpoints

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -10,7 +10,7 @@ const uadyfonApi = Router()
 //ruta principal
 uadyfonApi.get('/',(req, resp) => {
     //code here
-    resp.status(200).json({message:"Dentro de la api"})
+    resp.status(200).json({message:"Dentro de la api uadyfon"})
 })
 
 //endpoints
